cefea0e110e02dd4a91c53586dd4f8bc5fd5ec00
David Huu Pham <dayvidpham@gmail.com>
2024-10-20 02:03:31 -0700

    wip(server): attempts to load X.509 certs with wrong params
    * need to generate personal key-pair for each entity who needs TLS auth, generate a CSR signed with personal private key, then send signed CSR off to OCI CA to issue the certificate

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "crypto/tls"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	cert, err := tls.LoadX509KeyPair("./certs/ca.pem", "./certs/ca.pub")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cert)
 }
