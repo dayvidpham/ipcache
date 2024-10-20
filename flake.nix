02e3ea1e9e26431be59006d0f8c49c1b26f9203d
David Huu Pham <dayvidpham@gmail.com>
2024-10-19 20:14:32 -0700

    feat(nix): add Nix Flake devShell, direnv, can run a Go program

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Nix Develop Scaffolder";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.go
+          ];
+
+          inputsFrom = [
+            pkgs.go
+          ];
+
+
+          #nativeBuildInputs = [
+          #  pkgs.stdenv
+          #];
+
+          #buildInputs = [
+          #  pkgs.go
+          #  pkgs.gotools
+          #  pkgs.golangci-lint
+          #  pkgs.gopls
+          #  pkgs.go-outline
+          #  pkgs.gopkgs
+          #  pkgs.godef
+          #  pkgs.gocode-gomod
+          #  pkgs.gocode
+          #];
+        };
+
+        #package = pkgs.buildGoModule {
+        #  src = ./.;
+        #};
+      });
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dayvidpham/ipcache
+
+go 1.22.6
