c781009459255e2a8a4edba8898fbed19796082d
David Huu Pham <dayvidpham@gmail.com>
2024-11-04 02:08:26 -0800

    feat(clientd): requires some CLI flags to be passed now

--- cmd/clientd/clientd.go
+++ cmd/clientd/clientd.go
@@ -4,27 +4,70 @@ import (
 	"bufio"
 	"crypto/tls"
 	"crypto/x509"
+	"flag"
 	"fmt"
 	"log"
-	"strings"
-
-	//"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	/**
-	 * Required flags:
-	 * --server <hostname | IP (probably just tls.Dial format)>
-	 * --cert
-	 * --privatekey?
-	 * --publickey (probably don't need)
-	 */
+//////////////////////////////////////////////////////////////
+// Declare and init basic CLI flags
+//////////////////////////////////////////////////////////////
 
-	// Referencing https://gist.github.com/denji/12b3a568f092ab951456
+/**
+ * Required flags:
+ * --server <hostname | IP (probably just tls.Dial format)>
+ * --port <positive number>
+ * --cert <path>
+ * --privatekey <path>
+ * --publickey <path> (probably don't need)
+ *
+ * --server root CAs
+ */
+
+var parsedServer string
+var parsedPort uint
+var parsedCertPath string
+var parsedPrivatekeyPath string
+var parsedServerRootCACert string
+var nflagsRequired int = 5
+
+func init() {
+	flag.StringVar(&parsedServer, "server", "", "server to connect to; examples <ipcache.com | 192.168.0.1> ")
+	flag.UintVar(&parsedPort, "port", 0, "server port to connect to; examples <8080 | 4430>")
+	flag.StringVar(&parsedCertPath, "cert", "", "path to your certificate")
+	flag.StringVar(&parsedPrivatekeyPath, "privatekey", "", "path to your private key that was used to sign your certificate")
+	flag.StringVar(&parsedServerRootCACert, "server-root-ca-cert", "", "path to the expected server root CA certificate, used for verification")
+}
+
+func main() {
 	log.SetFlags(log.Lshortfile)
 
-	caCert, err := os.ReadFile("certs/self.pem")
+	///////////////////////////////
+	// Proccess flags
+	///////////////////////////////
+	flag.Parse()
+	log.Println("[DEBUG] --server", parsedServer)
+	log.Println("[DEBUG] --port", parsedPort)
+	log.Println("[DEBUG] --cert", parsedCertPath)
+	log.Println("[DEBUG] --privatekey", parsedPrivatekeyPath)
+	log.Println("[DEBUG] --server-root-ca-cert", parsedServerRootCACert)
+
+	// NOTE: Want some data type binding (var, flagname, Flag) for convenience error-checking
+	// Could also maybe use the Visitor for error-checking?
+	if flag.NFlag() < nflagsRequired {
+		log.Fatalf("[FATAL] Require at least %d flags to be set, but were only given %d\n", nflagsRequired, flag.NFlag())
+	}
+
+	parsedServerAddr := fmt.Sprintf("%s:%d", parsedServer, parsedPort)
+
+	///////////////////////////////
+	// Main client daemon program
+	// Referencing https://gist.github.com/denji/12b3a568f092ab951456
+	///////////////////////////////
+
+	caCert, err := os.ReadFile(parsedServerRootCACert)
 	if err != nil {
 		log.Println("[FATAL] Reading Root CA PEM file failed.\n\t- Reason:", err)
 		return
@@ -33,7 +76,7 @@ func main() {
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
 
-	cert, err := tls.LoadX509KeyPair("certs/self.pem", "certs/self.key")
+	cert, err := tls.LoadX509KeyPair(parsedCertPath, parsedPrivatekeyPath)
 	if err != nil {
 		log.Println("[FATAL] Loading X509 key pair failed.\n\t- Reason:", err)
 		return
@@ -44,10 +87,7 @@ func main() {
 		Certificates: []tls.Certificate{cert},
 	}
 
-	log.Println("Sending", len(config.Certificates), "certificates")
-	//log.Println("- the cert:", config.Certificates[0])
-
-	conn, err := tls.Dial("tcp", "127.0.0.1:4430", &config)
+	conn, err := tls.Dial("tcp", parsedServerAddr, &config)
 	if err != nil {
 		log.Println(err)
 		return
---
+++ run_clientd.sh
@@ -0,0 +1,6 @@
+./clientd \
+    --server 'localhost' \
+    --port '4430' \
+    --cert 'certs/self.pem' \
+    --privatekey 'certs/self.key' \
+    --server-root-ca-cert 'certs/self.pem'
